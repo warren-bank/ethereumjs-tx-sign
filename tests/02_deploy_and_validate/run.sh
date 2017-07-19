07d654b8c152d4fd67c30c1f57091880f058199d
Warren R. Bank <warren.r.bank@gmail.com>
2017-07-19 14:57:13 -0700

    update test runners: npm install dependencies if not already present

--- tests/02_deploy_and_validate/run.sh
+++ tests/02_deploy_and_validate/run.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
+# ----------------------------------------
+
 # lxterminal -e testrpc --account="0xe922354a3e5902b5ac474f3ff08a79cff43533826b8f451ae2190b65a9d26158,ffffffffffffffffff"
+
+# ----------------------------------------
+
+if [ ! -d "./js/node_modules" ]; then
+  echo 'installing npm dependencies for test...'
+  (cd ./js; npm install --production)
+
+  if [ $? -eq 0 ]; then
+    echo -e "\n"'installation of dependencies completed successfully.'
+  else
+    echo -e "\n"'installation of dependencies completed with errors.'
+    exit 1
+  fi
+fi
 
 # ----------------------------------------
 
